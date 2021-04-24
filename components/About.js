afd619487f0d075744d004c3ba53d550864581af
juanjose <jjsmm97@gmail.com>
2021-04-24 09:10:43 -0400

    add link

---
+++ components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="pt-8 container mx-auto px-4 md:px-0">
+      <h1 className="text-xl font-bold">Acerca de Nosotros</h1>
+      <p className="py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint aliquid provident maiores facilis corporis eius assumenda dolores nam deserunt aspernatur cum temporibus hic, esse facere quam. Voluptatum incidunt nisi blanditiis!</p>
+    </div>
+  )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import About from '../components/About'
 import Navbar from '../components/navbar'
 import SocialMedia from '../components/SocialMedia'
 
@@ -14,11 +15,14 @@ export default function Home() {
 
       <Navbar></Navbar>
       <div class="bg-fixed bg-cover bg-no-repeat bg-center h-screen w-full flex items-center" style={{backgroundImage: 'url(/bg_student.jpg)' }}>
-        <button className="py-4 px-6 text-2xl rounded-lg mx-auto bg-yellow-400 text-white hover:bg-yellow-300 hover:text-yellow-800">
+        <a type="button" 
+        className="py-4 px-6 text-2xl rounded-lg mx-auto transition duration-500 ease-in-out bg-yellow-400 text-white hover:bg-yellow-300 hover:text-yellow-800 transform hover:-translate-y-1 hover:scale-110" 
+        href="https://docs.google.com/forms/d/e/1FAIpQLSeul3XggoB9qO41nfKynuPObzXloKnZDNUycqFmB2taGuSb7Q/viewform" target="_blank">
           INSCRIPCION
-        </button>
+        </a>
         <SocialMedia></SocialMedia>
       </div>
+      <About></About>
       
     </div>
   );
