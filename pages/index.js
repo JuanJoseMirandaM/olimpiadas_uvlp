228169a02eb0ad4e8748ded75a42da69d4f78e7b
juanjose <jjsmm97@gmail.com>
2021-04-25 19:54:24 -0400

    add mensajes head

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import Typed from "react-typed";
 
 export default function Home() {
   // const bg_image = require('../assets/images/bg_student.jpg');
-  const textLines = ['OLIMPIADAS MEDICAS']
+  const textLines = ['OLIMPIADAS MEDICAS', '2da MINI OLIMPIADA', 'INSCRIPCIONES HASTA EL 9 DE MAYO']
   return (
     <div className="w-full mb-auto">
       <Head>
