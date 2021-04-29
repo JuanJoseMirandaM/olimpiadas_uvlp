e49baa7a6fe1e84db19cc44791771c530cb8ca7d
juanjose <jjsmm97@gmail.com>
2021-04-29 00:56:18 -0400

    add modal pdf

--- pages/olimpiadas/index.js
+++ pages/olimpiadas/index.js
@@ -4,14 +4,14 @@ import Link from 'next/link'
 const Olimpiadas = () => {
   return (
     <div className="w-full flex-grow container mx-auto">
-      <h2 className="w-full text-4xl lg:text-5xl font-bold pt-20 text-center uppercase">Olimpiadas</h2>
+      <h2 className="w-full text-4xl lg:text-5xl font-bold pt-24 text-center uppercase">Olimpiadas</h2>
       <div className="w-full mb-32">	
         <div className="h-1 mx-auto bg-yellow-500 w-96 opacity-50 my-0 py-0 rounded-t"></div>
       </div>
-      <div className="grid gap-x-10 gap-y-8 grid-cols-1 md:grid-cols-3">
-        <Link href="/olimpiadas/primera"><button className="h-16 w-full flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-pink-600 to-red-900 -rotate-1 sm:-rotate-2 text-2xl text-white text-center motion-safe hover:scale-125">1ra Mini Olimpiada</button></Link>
-        <Link href="/olimpiadas/segunda"><button className="h-16 w-full flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-yellow-400 to-yellow-600 rotate-1 sm:rotate-2 text-2xl text-white pt-4 text-center motion-safe hover:scale-125">2da Mini Olimpiada</button></Link>
-        <Link href="/olimpiadas/tercera"><button className="h-16 w-full flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-green-400 to-blue-300 -rotate-1 sm:-rotate-2 text-2xl text-white pt-4 text-center motion-safe hover:scale-125">3ra Mini Olimpiada</button></Link>
+      <div className="grid gap-x-4 gap-y-8 grid-cols-1 md:grid-cols-3">
+        <Link href="/olimpiadas/primera"><button className="h-28 w-3/4 flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-pink-600 to-red-900 -rotate-1 sm:-rotate-2 text-3xl text-white text-center font-bold motion-safe hover:scale-125">1ra MINI <br/> OLIMPIADA</button></Link>
+        <Link href="/olimpiadas/segunda"><button className="h-28 w-3/4 flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-yellow-400 to-yellow-600 rotate-1 sm:rotate-2 text-3xl text-white pt-4 text-center font-bold motion-safe hover:scale-125">2da MINI <br/> OLIMPIADA</button></Link>
+        <Link href="/olimpiadas/tercera"><button className="h-28 w-3/4 flex-none ml-full rounded-md transform shadow-lg bg-gradient-to-br from-green-400 to-blue-300 -rotate-1 sm:-rotate-2 text-3xl text-white pt-4 text-center font-bold motion-safe hover:scale-125">3ra MINI <br/> OLIMPIADA</button></Link>
       </div>
     </div>
   )
--- pages/olimpiadas/segunda.js
+++ pages/olimpiadas/segunda.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, useRef, useEffect, useState } from "react";
 import Link from 'next/link'
+import ModalPdf from '../../components/ModalPdf'
 
 const Segunda = () => {
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const cancelButtonRef = useRef();
+
+  const url1 = "https://drive.google.com/file/d/1KmBfHnCb2CVZID3PDIhJ4237PEvrmhHN/preview";
+  const url2 = "https://drive.google.com/file/d/1ffAn-W3A-WE8BNG2y5Sr_81GQGzefAer/preview";
+  const url3 = "https://drive.google.com/file/d/165sVlGQ-O2v9q9fQ2TfiPLep-qEtwvYt/preview";
+
+  function closeModal1() {
+    setOpen1(false);
+  }
+
+  function openModal1() {
+    setOpen1(true);
+  }
+
+  function closeModal2() {
+    setOpen2(false);
+  }
+
+  function openModal2() {
+    setOpen2(true);
+  }
+
+  function closeModal3() {
+    setOpen3(false);
+  }
+
+  function openModal3() {
+    setOpen3(true);
+  }
+
   return (
     <div className="w-full container px-4 md:px-0 mx-auto">
-      <h1 className="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-2 pt-20">Segunda Mini Olimpiada</h1>
-      <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-yellow-600 mb-3 uppercase">15, 22, 29 de Mayo</h2>
+      <h1 className="text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold text-gray-900 tracking-tight mb-2 pt-24">Segunda Mini Olimpiada</h1>
+      <h2 className="sm:text-lg sm:leading-snug font-semibold tracking-wide uppercase text-yellow-600 mb-3 uppercase">El 15, 22 y 29 de Mayo</h2>
       
       <div className="mt-8">
         <h3 className="text-3xl font-bold my-4">Convocatoria</h3>
@@ -26,8 +61,32 @@ const Segunda = () => {
       </div>
       <div className="mt-8">
         <h3 className="text-3xl font-bold my-4">Material de estudio</h3>
-        <div className="grid gap-x-10 gap-y-8 grid-cols-1 md:grid-cols-2">
-          <div className="flex flex-col items-center justify-center">
+        <div className="grid gap-x-10 gap-y-8 grid-cols-1 md:grid-cols-3">
+          <button
+            type="button"
+            onClick={openModal1}
+            className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            Fisiología Medica - GUSTON Y HALL
+          </button>
+
+          <button
+            type="button"
+            onClick={openModal2}
+            className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            Microbiología Medica - MURRAY
+          </button>
+
+          <button
+            type="button"
+            onClick={openModal3}
+            className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            Patología Estructural y Funcional - ROBBINS
+          </button>
+          
+          {/* <div className="flex flex-col items-center justify-center">
             <h4 className="text-xl font-bold my-4">Fisiología Medica - GUSTON Y HALL</h4>
             <iframe src="https://drive.google.com/file/d/1KmBfHnCb2CVZID3PDIhJ4237PEvrmhHN/preview" width="90%" height="480"></iframe>
           </div>
@@ -38,7 +97,7 @@ const Segunda = () => {
           <div className="flex flex-col items-center justify-center">
             <h4 className="text-xl font-bold my-4">Patología Estructural y Funcional - ROBBINS</h4>
             <iframe src="https://drive.google.com/file/d/165sVlGQ-O2v9q9fQ2TfiPLep-qEtwvYt/preview" width="90%" height="480"></iframe>
-          </div>
+          </div> */}
         </div>
         {/* <div>
           <object
@@ -51,7 +110,176 @@ const Segunda = () => {
         </div>
         <a href="https://www.mediafire.com/file/adg0zunp43ldpfn/AUXILIARES_DE_DOCENCIA_2021_INF.pdf/file" target="_blank">AUXILIARES_DE_DOCENCIA_2021_INF.pdf/file</a> */}
       </div>
-      
+      {/* <ModalPdf>
+        open={open1}
+        cancelButtonRef={cancelButtonRef}
+        closeModal={closeModal1}
+        url={url1}
+      </ModalPdf> */}
+      <Transition show={open1} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          static
+          open={open1}
+          onClose={closeModal1}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-2 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <iframe src={url1} width="100%" height="500"></iframe>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal1}
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <Transition show={open2} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          static
+          open={open2}
+          onClose={closeModal2}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-2 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <iframe src={url2} width="100%" height="500"></iframe>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal2}
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
+
+      <Transition show={open3} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          initialFocus={cancelButtonRef}
+          static
+          open={open3}
+          onClose={closeModal3}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="inline-block w-full max-w-md p-2 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                <iframe src={url3} width="100%" height="500"></iframe>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                    onClick={closeModal3}
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   )
 }
